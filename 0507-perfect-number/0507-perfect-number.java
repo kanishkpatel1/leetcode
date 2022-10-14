a5c9d9ad74993b42e7cdf9fdfa32ccae425d03fc
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2022-10-14 07:11:14 +0530

    Time: 3 ms (65.47%), Space: 41.4 MB (11.79%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=1;
+        if(num==1){
+            return false;
+        }
+        for(int i=2;i<Math.sqrt(num);i++){
+            if(num%i==0){
+           if (i * i == num) {
+                    sum += i;
+           }
+            else{
+                    sum += i + (num / i);
+                }
+            }
+        }
+        if(sum==num){
+            return true;
+        }
+        return false;
+    }
+}
