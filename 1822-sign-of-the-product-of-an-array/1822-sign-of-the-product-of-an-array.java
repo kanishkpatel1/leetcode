41e10860c67f09127fc783378ea1ddd4fed9508b
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2022-10-17 23:31:32 +0530

    Time: 3 ms (18.83%), Space: 43.5 MB (79.98%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int s=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                return 0;
+            }
+            else if(nums[i]<0){
+                s++;   // here we count how many times minus digit occured if it is occured even time then it is +ve
+            }
+        }
+          if(s%2==0){
+              return 1;
+          }
+        return -1;
+        }
+}
