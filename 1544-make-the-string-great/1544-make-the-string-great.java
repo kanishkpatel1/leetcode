89c7ed71344f15a80a5de0345c16a80e30081a07
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2022-11-08 08:46:19 +0530

    Time: 2 ms (98.79%), Space: 42.1 MB (89.62%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,7 +1,8 @@
 class Solution {
     public String makeGood(String s) {
         for(int i=0;i<s.length()-1;i++){
-            if(s.charAt(i)+32==s.charAt(i+1)||s.charAt(i)==s.charAt(i+1)+32){
+          //  if(s.charAt(i)+32==s.charAt(i+1)||s.charAt(i)==s.charAt(i+1)+32){  // or below line
+            if(Math.abs(s.charAt(i)-s.charAt(i+1))==32){
                 return makeGood(s.substring(0,i)+s.substring(i+2));
             }
         }
