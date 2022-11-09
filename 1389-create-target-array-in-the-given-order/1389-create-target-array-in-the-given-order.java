69f7470b3ad3f835d4b1df7e55dd8f9483126a13
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2022-11-09 08:17:39 +0530

    Time: 1 ms (85.27%), Space: 43 MB (8.66%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -1,32 +1,30 @@
 class Solution {
-    int[]  numsInsert(int[] arr,int index,int val ){
-          for(int k=arr.length-1;k>index;--k){
-                    arr[k]=arr[k-1];
-                   
-                }
-                 arr[index]=val;
+
+    int[] numsInsert(int[] arr, int index, int val) {
+        for (int k = arr.length - 1; k > index; --k) {
+            arr[k] = arr[k - 1];
+        }
+        arr[index] = val;
         return arr;
     }
-    
+
     public int[] createTargetArray(int[] nums, int[] index) {
-        int n=nums.length;
-        int arr[]=new int[n];
-        for(int i=0;i<n;++i){    // creating an array with the same length initialized with -1
-            arr[i]=-1;              
-            }
-        for(int i=0,j=0;i<n;++i){
-            if(arr[index[i]]==-1){
-                arr[index[i]]=nums[j++];
-            }
-            else{     // if index is repeated then we should insert the element
-                //and shift the rest of the array 
-                arr=numsInsert(arr,index[i],nums[j++]);
+        int n = nums.length;
+        int arr[] = new int[n];
+        for (int i = 0; i < n; ++i) { // creating an array with the same length initialized with -1
+            arr[i] = -1;
+        }
+        for (int i = 0, j = 0; i < n; ++i) {
+            if (arr[index[i]] == -1) {
+                arr[index[i]] = nums[j++];
+            } else { // if index is repeated then we should insert the element
+                //and shift the rest of the array
+                arr = numsInsert(arr, index[i], nums[j++]);
                 // function called numsInsert for doing our work shift Array
-              
+
             }
         }
-        
-        
+
         return arr;
     }
 }
