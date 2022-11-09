1138729c8c459e8cb360821b7cf82ab55c64e82a
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2022-11-10 00:19:34 +0530

    Time: 5 ms (45.55%), Space: 43.4 MB (14.11%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.java
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -2,14 +2,14 @@ class Solution {
     public String truncateSentence(String s, int k) {
        String[] str=s.split(" ");
         String ret="";
-        for(int i=0;i<k-1;i++){
+        for(int i=0;i<k-1;++i){
             ret+=str[i];
     
             ret+=" ";
             
             
         }
-        ret+=str[k-1];
+        ret+=str[k-1];  // for last word that are left because of space 
         return ret;
     }
 }
