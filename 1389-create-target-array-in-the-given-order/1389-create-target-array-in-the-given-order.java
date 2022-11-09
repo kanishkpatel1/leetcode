25786f63cf2343b2a40aea54ec0314cc413e8fe9
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2022-11-09 08:09:32 +0530

    Time: 2 ms (26.51%), Space: 42.4 MB (51.56%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -2,16 +2,16 @@ class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
         int n=nums.length;
         int arr[]=new int[n];
-        for(int i=0;i<n;i++){    // creating an array with the same length initialized with -1
+        for(int i=0;i<n;++i){    // creating an array with the same length initialized with -1
             arr[i]=-1;              
             }
-        for(int i=0,j=0;i<n;i++){
+        for(int i=0,j=0;i<n;++i){
             if(arr[index[i]]==-1){
                 arr[index[i]]=nums[j++];
             }
             else{     // if index is repeated then we should insert the element
                 //and shift the rest of the array 
-                for(int k=arr.length-1;k>index[i];k--){
+                for(int k=arr.length-1;k>index[i];--k){
                     arr[k]=arr[k-1];
                    
                 }
