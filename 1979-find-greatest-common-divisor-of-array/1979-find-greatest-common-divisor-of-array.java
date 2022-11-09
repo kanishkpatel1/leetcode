845fcdd11fa80540ca0f670d1724076950e9eebc
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2022-11-09 20:36:59 +0530

    Time: 4 ms (32.88%), Space: 43.8 MB (41.89%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -5,7 +5,9 @@ class Solution {
        // int max=1;
         int a = nums[0];
         int b = nums[nums.length - 1];
-        for (int i = a; i >= 2; i--) {
+        for (int i = a; i >= 2; i--) {  
+            // because we have to return the greatest common divisor of array 
+            //and if it not then return 1 because 1 is divisible by both
             if (a % i == 0 && b % i == 0) {
                 return i;
             }
