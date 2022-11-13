2f2d19790b6c1d20f9fd27f72b25e8d07f623a5a
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2022-11-13 19:14:55 +0530

    Time: 2 ms (55.65%), Space: 42.8 MB (18.39%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -3,9 +3,11 @@ public class Solution {
     public int reverseBits(int n) {
         int rev=0;
         for(int i=0;i<32;i++){
-            rev <<=1;
-            rev |=(n&1);
-            n>>=1;
+            rev <<=1; 
+            rev |=(n&1);  //append the last with in the rev
+            //(get the last bit with binary and )
+            // append it with binary in the rev variable
+            n>>=1;  // remove the last bit from the number 
             
         }
        return rev; 
