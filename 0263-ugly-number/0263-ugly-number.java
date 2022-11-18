615128fd4dce34198b7f0f967c0dd69da237b27a
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2022-11-18 09:20:19 +0530

    Time: 2 ms (80.60%), Space: 41.3 MB (52.23%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,22 +1,49 @@
+// class Solution {
+//     public boolean isUgly(int n) {
+//       if (n == 1)
+//             return true;
+//         if (n <= 0)
+//             return false;
+ 
+//         // Condition to check if
+//         // a number is divide by
+//         // 2, 3, or 5
+//         if (n % 2 == 0) {
+//             return (isUgly(n / 2));
+//         }
+//         if (n % 3 == 0) {
+//             return (isUgly(n / 3));
+//         }
+//         if (n % 5 == 0) {
+//             return (isUgly(n / 5));
+//         }
+//         return false;
+//     }  
+//     }
+
 class Solution {
     public boolean isUgly(int n) {
-      if (n == 1)
-            return true;
-        if (n <= 0)
+        if(n<=0){
             return false;
- 
-        // Condition to check if
-        // a number is divide by
-        // 2, 3, or 5
-        if (n % 2 == 0) {
-            return (isUgly(n / 2));
         }
-        if (n % 3 == 0) {
-            return (isUgly(n / 3));
+        if(n==1){
+            return true;
         }
-        if (n % 5 == 0) {
-            return (isUgly(n / 5));
+        while(n>2){
+            if(n%2==0){
+                n=n/2;
+            }
+            else if(n%3==0){
+                n=n/3;
+            }
+            else if(n%5==0){
+                n=n/5;
+            }
+            else{
+                return false;
+            }
+            
         }
-        return false;
-    }  
+   return true;
     }
+}
