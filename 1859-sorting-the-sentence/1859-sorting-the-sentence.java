df72aecaa3a2ad3adeae174d9413dbaec128c79e
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2022-11-20 23:38:46 +0530

    Time: 0 ms (100.00%), Space: 40 MB (97.92%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -3,12 +3,14 @@ class Solution {
                String []arr=s.split(" ");
         String []str=new String[arr.length];
         for(int i=0;i<arr.length;i++){
-            int a=arr[i].length();
-            int b=arr[i].charAt(a-1)-'0';
-            String sub = arr[i].substring(0,a-1);
-            str[b-1]=sub;
+            int a=arr[i].length(); // getting the length of each words
+            int b=arr[i].charAt(a-1)-'0'; 
+            // to extract the position no from words
+            String sub = arr[i].substring(0,a-1); 
+            // remove number (last digit) from string 
+            str[b-1]=sub; // store it in new array
         }
-        String res=String.join(" ", str);
+        String res=String.join(" ", str);  // join the result string 
         return res;
     }
 }
