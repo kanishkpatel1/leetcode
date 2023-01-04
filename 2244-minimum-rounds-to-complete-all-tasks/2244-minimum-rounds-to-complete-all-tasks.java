e03fc8d5fa7c68c95e2ac8008ccb5d2c0e49ea7a
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-01-04 16:51:54 +0530

    Time: 58 ms (60.70%), Space: 104.8 MB (34.61%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumRounds(int[] tasks) {
+       HashMap<Integer, Integer> map = new HashMap<>();
+        for(int x:tasks){
+            map.put(x, map.getOrDefault(x,0)+1);
+        }
+        int res=0;
+        for(int val: map.values()){
+            if(val==1){
+                return -1;
+            }
+            res+=(val+2)/3;
+// this formula will return the res according to our question if we dont want to use it  we can use below lines code so comment above line and uncomment below 6 lines
+            // else if(val%3==0){
+            //     res+=(val/3);
+            // }
+            // else{
+            //     res+=val/3+1;
+            // }
+        }
+        return res;
+    }
+}
