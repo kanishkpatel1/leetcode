ba7233c078940482080a1fb8016ee84f56db2a1f
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-01-06 19:04:34 +0530

    Time: 22 ms (61.72%), Space: 54 MB (45.05%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,10 +1,16 @@
+// In this code first we calculate the number of ones in present  line poc(previous one count) and then multiply that with the number of ones in the another lines  coc (current one count)
+
+// because the number of laser beams is equal to the ===> coc*poc 
+
 class Solution {
     public int numberOfBeams(String[] bank) {
       
         int poc=0;
+        // poc: previous one count
         int ans=0;
         for(String s:bank){
             int coc=0;
+            // coc: current one count
             for(int i=0;i<s.length();i++){
                 if(s.charAt(i)=='1'){
                     coc++;
