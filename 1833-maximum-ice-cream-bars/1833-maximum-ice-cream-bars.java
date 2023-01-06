db9308a8f01a1a8ce69e6278eb9f9dee781177b1
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-01-06 16:52:50 +0530

    Time: 44 ms (50.47%), Space: 74.5 MB (38.15%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maxIceCream(int[] nums, int coins) {
       Arrays.sort(nums);
+        // first we will sort the prices so that buy that icecream first that is less costly
          int sum=0;
         for(int i=0;i<nums.length;i++){
                    if(coins>=nums[i]){
