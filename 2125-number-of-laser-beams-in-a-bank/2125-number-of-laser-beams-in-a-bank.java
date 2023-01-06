b9754632eae44968cef108b0600e33d2cb5d12a6
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-01-06 19:01:35 +0530

    Time: 38 ms (17.22%), Space: 54.2 MB (37.91%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numberOfBeams(String[] bank) {
+      
+        int poc=0;
+        int ans=0;
+        for(String s:bank){
+            int coc=0;
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i)=='1'){
+                    coc++;
+           }
+            }
+             if(coc>0){
+                ans+=coc*poc;
+                poc=coc;
+                    
+                }
+        }
+        return ans;
+    }
+}
