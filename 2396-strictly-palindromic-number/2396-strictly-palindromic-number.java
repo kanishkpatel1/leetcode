3647113563653dc7cab736ab768870afc3ff3a5c
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-01-08 00:29:09 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (63.07%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,5 +1,28 @@
+// class Solution {
+//     public boolean isStrictlyPalindromic(int n) {
+//         return false;
+//     }
+// }
+
+// only return false will also work for this code
+
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+ 
+         for (int j = 2; j <= n - 2; j++) {
+		   String str = Integer.toString(n, j);   // for converting the number n to the base i 
+
+		   int i = 0, length = str.length() - 1;
+		   while (i <= length / 2) {
+			   if (str.charAt(i) != str.charAt(length)) {
+				   return false;
+			   }
+			   i++;
+			   length--;
+		   }
+	   }
+		return true;
     }
+    
+    
 }
