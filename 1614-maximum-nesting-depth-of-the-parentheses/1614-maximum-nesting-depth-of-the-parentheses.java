8018b5d77670e45aa00df061368913d38a79be88
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-02-04 00:39:33 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (77.33%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -9,7 +9,9 @@ class Solution {
                 sum++;
             }
             else if(ch[i]==')'){
-                max=Math.max(sum,max);
+                max=Math.max(sum,max);  // this is when there is not any chars between the braces 
+                //"(1())"  op-> 2
+                //"()" op-> 1
                 sum--;
             }
             
