b2944bdeae95d29b52968644f4980b9442106673
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-02-06 22:10:31 +0530

    Time: 14 ms (78.75%), Space: 50.5 MB (39.57%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -2,19 +2,19 @@ class MyHashSet {
     
     boolean arr[];
     public MyHashSet() {
-        arr=new boolean[1000000+1];
+        arr=new boolean[1000000+1];  // as per the que we can have as many 10^6 keys so use 10^6+1 index boolean array
     }
     
     public void add(int key) {
-        arr[key]=true;
+        arr[key]=true;  // put true at index that means key present
     }
     
     public void remove(int key) {
-        arr[key]=false;
+        arr[key]=false;       // put false at index that means key is not present
     }
     
     public boolean contains(int key) {
-        return arr[key];
+        return arr[key];  // if key present return true else return false
     }
 }
 
