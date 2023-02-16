3ddd5bcba1275b1ad087a02b5ef16bd884a791a6
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-02-16 22:34:07 +0530

    Time: 14 ms (99.75%), Space: 51.4 MB (50.26%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,14 +1,17 @@
-// In this code maintain a sliding window from i to k and if any elements find duplicate return true 
+// In this code maintain a sliding window from i to k and if any elements find duplicate (from index i to k) return true 
 // else return false
 
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // Use Hashset to add Integers
         HashSet<Integer> hs=new HashSet<>();
         for(int i=0;i<nums.length;i++){
         if(i>k){
-        hs.remove(nums[i-k-1]);  //remove element out of sliding window    
+        hs.remove(nums[i-k-1]);  
+//remove element out of sliding window (when slidin window full)    
         }
-            if(!hs.add(nums[i])){
+            if(!hs.add(nums[i])){ 
+    // if any element will not be added in the set it means that it is already in the set (and it is within range from i-k ) so return true
                 return true;
             }
         }
