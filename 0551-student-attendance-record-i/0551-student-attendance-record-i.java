171054357b25900a155f3a590133c1b74163c633
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-03-24 07:12:03 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (46.17%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        char [] ch=s.toCharArray();
+        int lcount=0,acount=0;
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]=='L'){
+                lcount++;
+                if(lcount==3){
+                    return false;
+                }
+                
+            }
+            else if(ch[i]=='A'){
+                lcount=0;
+                acount++;
+                if(acount>=2){
+                    return false;
+                }
+            }
+            else{
+                lcount=0;
+            }
+        }
+        return true;
+    }
+}
