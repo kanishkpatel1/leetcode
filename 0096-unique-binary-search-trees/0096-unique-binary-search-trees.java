02a05dd6a41af4d93dc56e4ff8d12226bae2c2b2
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-03-28 15:28:37 +0530

    Time: 2231 ms (9.71%), Space: 39.3 MB (49.53%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+    int catalon(int n){
+        int res=0;
+        if(n<=1){
+            return 1;
+        }
+        for(int i=0;i<n;i++){
+           res+=catalon(i)*catalon(n-i-1); 
+        }
+        return res;
+        
+    }
+    public int numTrees(int n) {
+        return catalon(n);
+        
+    }
+}
