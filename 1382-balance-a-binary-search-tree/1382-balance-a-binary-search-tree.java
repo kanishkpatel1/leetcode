5a4d54b36aad730deb18d9bdd4341c5bceaf047b
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-04-02 16:04:07 +0530

    Time: 2 ms (99.88%), Space: 45.4 MB (75.91%) - LeetHub

---
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     List<TreeNode> ls=new ArrayList<>();
+    public TreeNode balanceBST(TreeNode root) {
+       helper(root);
+        int start=0;
+        int end=ls.size()-1;
+      return  createBBST(start,end);
+        
+    }
+    public void helper(TreeNode root){
+        if(root==null){
+            return;
+        }
+        helper(root.left);
+        ls.add(root);
+        helper(root.right);
+    }
+    
+    public TreeNode createBBST(int start,int end){
+        if(start>end){
+            return null;
+        }
+        int mid=start+(end-start)/2;
+        TreeNode root=ls.get(mid);
+        root.left=createBBST(start,mid-1);
+        root.right=createBBST(mid+1,end);
+        
+        return root;
+    }
+}
