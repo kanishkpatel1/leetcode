96c15943cbbc01c37d81248c8dc50dddf5b527c3
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-04-06 22:18:18 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (65.52%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -1,6 +1,8 @@
 class Solution {
     public String convertToTitle(int n) {
+        
         // Take a pen and paper and try to dry run you will understant the logic behind this question
+        // And Also try to solve question 171 same logic 
         StringBuilder sb=new StringBuilder();
         while(n>0){
             n--;
