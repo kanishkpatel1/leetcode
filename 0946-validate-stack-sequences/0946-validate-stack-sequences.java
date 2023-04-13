46133c9b429fa728922b84e4341cfca57c0e01c5
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-04-13 08:21:49 +0530

    Time: 3 ms (85.90%), Space: 42.4 MB (33.76%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.java
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -4,8 +4,12 @@ class Solution {
         int j=0;
         Stack<Integer> stk=new Stack<>();
         for(int i=0;i<n;i++){
-            stk.push(pushed[i]);
-            while(!stk.empty() && stk.peek()==popped[j]){
+            stk.push(pushed[i]);// first we will have to  store the value in stack 
+            while(!stk.empty() && stk.peek()==popped[j]){ 
+// check if the last stored value in the stack is matched with the popped[j] 
+//if matched then remove value from stack and increase the j counter to check again and again 
+//till the stack not empty after all operations if the stack is empty then return true
+                
                 stk.pop();
                 j++;
             }
