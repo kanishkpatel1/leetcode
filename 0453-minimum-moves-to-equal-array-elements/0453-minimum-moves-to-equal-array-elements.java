9ab882e47d3d830a0005bf4166c111651625895f
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-04-30 23:55:00 +0530

    Time: 2 ms (79.11%), Space: 43.2 MB (61.19%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
@@ -3,6 +3,8 @@ class Solution {
     public int minMoves(int[] nums) {
         int ans=0;
         int min = nums[0];
+        // In it we use simple approach first calculate the min number of the array and after that find the difference of each ele 
+        // with the min number because in each step we can increment only 1 times so calculate sum of difference that is our ans
         for (int i = 0; i < nums.length; i++) {
             min = Math.min(min, nums[i]);
         }
