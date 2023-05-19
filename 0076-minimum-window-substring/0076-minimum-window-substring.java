8b4fb6330bc7a6ab3e944bcbf94bfb3f5a5e5811
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-05-19 16:52:50 +0530

    Time: 221 ms (11.75%), Space: 44.8 MB (5.29%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String minWindow(String s1, String t) {
+        String ans="";
+        HashMap<Character,Integer> map2=new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            map2.put(ch,map2.getOrDefault(ch,0)+1);
+        }
+        int mct=0; // match count
+        int dmct=t.length(); // default match count
+        HashMap<Character,Integer> map1=new HashMap<>();
+        int i=-1;
+        int j=-1;
+        while(true){
+            boolean f1=false;
+            boolean f2=false;
+            //acquire
+            while(i<s1.length()-1 && mct<dmct){
+                i++;
+                char ch=s1.charAt(i);
+                map1.put(ch,map1.getOrDefault(ch,0)+1);
+                if(map1.getOrDefault(ch,0)<=map2.getOrDefault(ch,0)){
+                    mct++;
+                }
+                f1=true;
+            }
+            
+            // collect answers and release
+            while(j<i && mct==dmct){
+                String tempans=s1.substring(j+1,i+1);
+                if(ans.length()==0 || tempans.length()<ans.length()){
+                    ans=tempans;
+                }
+                j++;
+                char ch=s1.charAt(j);
+                if(map1.get(ch)==1){
+                    map1.remove(ch);
+                }
+                else{
+                    map1.put(ch,map1.get(ch)-1);
+                }
+                if(map1.getOrDefault(ch,0)<map2.getOrDefault(ch,0)){
+                    mct--;
+                }
+                f2=true;
+            }
+            if(f1==false && f2==false){
+                break;
+            }
+        }
+        return ans;
+    }
+}
