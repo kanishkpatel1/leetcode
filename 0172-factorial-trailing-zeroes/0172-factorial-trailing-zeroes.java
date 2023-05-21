b81e6c7d7deb58fab2fa280280582de91a11d71f
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-05-21 20:29:38 +0530

    Time: 0 ms (100.00%), Space: 40 MB (27.20%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,13 +1,22 @@
-class Solution {
-    public int trailingZeroes(int fact) {
-        int count=0;
-        while(fact>=5){   
-            // because if we multiply 2 with 5 then we get one zero so
-            //count the 5
-            fact=fact/5;
-            count+=fact;
+// class Solution {
 
-        }
-        return count;
+//     public int trailingZeroes(int fact) {
+//         int count = 0;
+//         while (fact >= 5) {
+//             // because if we multiply 2 with 5 then we get one zero so
+//             //count the 5
+//             fact = fact / 5;
+//             count += fact;
+//         }
+//         return count;
+//     }
+// }
+
+// Using recursion one line solution
+
+class Solution {
+
+    public int trailingZeroes(int fact) {
+    return fact == 0 ? 0 : fact / 5 + trailingZeroes(fact /5);
     }
 }
