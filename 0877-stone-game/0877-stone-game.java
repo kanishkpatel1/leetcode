e79a33b01f59e906b1c87a6f71415e76432e0756
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-05-25 20:42:13 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (60.32%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
+        // sum of all the piles is odd so sum of even and sum of odd will never be equal and one of them always high 
+        // so alice pick the  highest so every time alice will win the game 
         return true;
     }
 }
