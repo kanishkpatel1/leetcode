6e28a073f575cc6f53a3283249dc997cf5f5cf48
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-06-02 22:25:56 +0530

    Time: 3 ms (69.83%), Space: 40.6 MB (94.22%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -7,15 +7,21 @@ class Solution {
         int b=0;
         int c=0;
         
-        for(int i=1;i<n;i++){
-            int two=dp[a]*2;
-            int three=dp[b]*3;
-            int five=dp[c]*5;
-            dp[i]=Math.min(two,Math.min(three,five));
-            if(two==dp[i])a++;
-            if(three==dp[i])b++;
+        // 
+        for(int i=1;i<n;i++){ 
+// multiply krne ke value hameskha dp ki table se uthayenge jisse  not ugly wala milne ka chance hi na rhe 
+// because agr 42 hai to 2 se divide hone ke baad 21 bchega matlab 2*21 ya 3*7 ya 2*7 in cases se bachne ke liye dp table se value uthake multiply krenge bss
+            
+            int two=dp[a]*2; // multiple of 2
+            int three=dp[b]*3;   // multiple of 2
+            int five=dp[c]*5;     // multiple of 2
+            
+            dp[i]=Math.min(two,Math.min(three,five));  // store the minimum number among three4
+            
+            if(two==dp[i])a++;   // increase the pointer jiska answer tumne store kiya hai agr do same r
+            if(three==dp[i])b++;  // hai to dono ka pointer increase krdo 
             if(five==dp[i])c++;
         }
-        return dp[n-1];
+        return dp[n-1];  // last wala element array se return krdo wahi answer hai bhai
     }
 }
