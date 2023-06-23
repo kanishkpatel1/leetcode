ecf5ea5af50fc6e2d3a5250494c05cbc28b94ec0
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-06-23 21:45:15 +0530

    Time: 1 ms (95.27%), Space: 40.7 MB (40.22%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,16 +1,44 @@
+// BruteForce Solution 
+// class Solution {
+//     public int minimumRecolors(String blocks, int k) {
+//         int ans=Integer.MAX_VALUE;
+//         for(int i=0;i<=blocks.length()-k;i++){
+//            int currcount=0;
+//            for(int j=i;j<i+k;j++){
+//                if(blocks.charAt(j)=='W'){
+//                    currcount++;
+                   
+//                }
+//              }
+//            ans=Math.min(ans,currcount);
+//        }
+//         return ans;
+//     }
+// }
+
+// Sliding window approach
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int ans=Integer.MAX_VALUE;
-        for(int i=0;i<=blocks.length()-k;i++){
-           int currcount=0;
-           for(int j=i;j<i+k;j++){
-               if(blocks.charAt(j)=='W'){
-                   currcount++;
-                   
-               }
-             }
-           ans=Math.min(ans,currcount);
-       }
-        return ans;
+         int ans=Integer.MAX_VALUE;
+        int count=0;
+        for(int i=0;i<k;i++){ // calculate 'W' for sliding window k
+                if(blocks.charAt(i)=='W'){
+                    count++;
+                }
+            }
+        int leftptr=0;
+        int rightptr=k;
+        ans=Math.min(ans,count);
+        while(rightptr<blocks.length()){
+            if(blocks.charAt(leftptr++)=='W') count--;  // remove one element from sliding window
+            if(blocks.charAt(rightptr++)=='W') count++;  // add one item in sliding window
+            if(count==0){
+                return 0;
+            }
+            ans=Math.min(ans,count);
+        }
+        
+         return ans;
+            }
+      
     }
-}
