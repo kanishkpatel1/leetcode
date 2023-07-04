b2ad671b9328d65c731a4cbe749095f0aaaae940
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-07-04 13:22:34 +0530

    Time: 3 ms (98.52%), Space: 9.4 MB (98.52%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,18 +1,33 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         unordered_map<int, int> map;
+        
+//         for (int i = 0; i < nums.size(); i++) {
+//             map[nums[i]] = map[nums[i]] + 1;
+//         }
+        
+//         for (auto& pair : map) {
+//             if (pair.second == 1) {
+//                 return pair.first;
+//             }
+//         }
+        
+//         return -1;
+//     }
+// };
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> map;
+    int singleNumber(vector<int>& arr) {
+        int ones = 0;
+        int twos = 0;
         
-        for (int i = 0; i < nums.size(); i++) {
-            map[nums[i]] = map[nums[i]] + 1;
+        for (int i = 0; i < arr.size(); i++) {
+            int el = arr[i];
+            ones = (ones ^ el) & (~twos);
+            twos = (twos ^ el) & (~ones);
         }
         
-        for (auto& pair : map) {
-            if (pair.second == 1) {
-                return pair.first;
-            }
-        }
-        
-        return -1;
+        return ones;
     }
 };
