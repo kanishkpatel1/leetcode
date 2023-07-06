f97ad414b1fe2088cf4cc42a5a33e36cc87ea576
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-07-06 14:34:49 +0530

    Time: 306 ms (89.38%), Space: 55.5 MB (80.00%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.java
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.java
@@ -1,3 +1,14 @@
+// In this question add 1 in the min ele of the array every time 
+// for ex -->  1 1 1 1 1 value of k =3
+// step -1:    2 1 1 1 1
+// step -2:    2 2 1 1 1
+// step -3:    2 2 2 1 1
+// dry run same for another test case you will get the logic
+// multiplication of this will always be maximum
+// so maintain a priority que and add 1 untill the value of k be 0
+// 
+// 
+
 class Solution {
          public static int maximumProduct(int[] nums, int k) {
         PriorityQueue<Integer>pq=new PriorityQueue<>();
