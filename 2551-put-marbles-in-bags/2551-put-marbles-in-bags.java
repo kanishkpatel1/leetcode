d96a7279dd76810c1f9161d5716adc5d882620c4
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-07-08 12:58:08 +0530

    Time: 36 ms (100.00%), Space: 59.8 MB (21.59%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.java
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.java
@@ -1,15 +1,41 @@
-class Solution {
+// take sum of two elements each pair
+// and then sort 
+// and the take two vairable left sum and right sum 
+// sum upto (k-1) from both sides 
+// then return right sum -left sum
+
+// class Solution 
+// {
+//     public long putMarbles(int[] weights, int k) {
+//         List<Integer>ls=new ArrayList<>();
+//         for(int i=1;i<weights.length;i++){
+//             ls.add(weights[i-1]+weights[i]);
+//         }
+//         Collections.sort(ls);
+//         long leftsum=0;
+//         long rightsum=0;
+//         for(int i=0;i<k-1;i++){
+//             leftsum+=ls.get(i);
+//             rightsum+=ls.get(ls.size()-1-i);
+//         }
+//         return rightsum-leftsum;
+//     }
+// }
+
+
+class Solution 
+   {
     public long putMarbles(int[] weights, int k) {
-        List<Integer>ls=new ArrayList<>();
+        int arr[]=new int[weights.length-1];
         for(int i=1;i<weights.length;i++){
-            ls.add(weights[i-1]+weights[i]);
+            arr[i-1]=weights[i-1]+weights[i];
         }
-        Collections.sort(ls);
+        Arrays.sort(arr);
         long leftsum=0;
         long rightsum=0;
         for(int i=0;i<k-1;i++){
-            leftsum+=ls.get(i);
-            rightsum+=ls.get(ls.size()-1-i);
+            leftsum+=arr[i];
+            rightsum+=arr[arr.length-1-i];
         }
         return rightsum-leftsum;
     }
