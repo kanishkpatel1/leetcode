0d5b256b6275f902765b900b2a15ce57ee0caf80
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-09-10 05:54:39 +0530

    Time: 1 ms (72.40%), Space: 39.7 MB (73.05%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,33 @@
+// // recursion will give tle
+// TC-> O(N^target)
+// class Solution {
+//     public int combinationSum4(int[] nums, int target) {
+//       if(target==0) {
+//           return 1;
+//       }
+//         int res=0;
+//         for(int i:nums){
+//             if(i<=target){
+//                 res+=combinationSum4(nums,target-i);
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+// Using dp
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int dp[]=new int[target+1];
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            for(int n:nums){
+                if(i>=n){
+                    dp[i]+=dp[i-n];
+                }
+            }
+        }
+        return dp[target];
+        
+    }
+}
