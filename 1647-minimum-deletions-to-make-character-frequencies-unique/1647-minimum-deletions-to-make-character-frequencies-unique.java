21713e4e49a904adcc2f35d87755bca6f97414ee
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-09-12 08:51:59 +0530

    Time: 9 ms (75.24%), Space: 44.5 MB (65.10%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,13 +1,18 @@
 class Solution {
     public int minDeletions(String s) {
        int freq[]=new int[26];
+        // calculate freq of each chars
         for(int i=0;i<s.length();i++){
             freq[s.charAt(i)-'a']++;
         }
         int ans=0;
+        
         HashSet<Integer> hs=new HashSet<>();
+        
         for(int i=0;i<26;i++){
             while(freq[i]>0){
+                // check if the freq of the current char is present in the hs or not 
+                // if present decrease it until the freq[i] become 0 or not contains in hs 
                 if(hs.contains(freq[i])){
                     freq[i]--;
                     ans++;
@@ -16,6 +21,7 @@ class Solution {
                     break;
                 }
             }
+            // after decreasing the frequency if it is not 0 then push it to the hashset
             if(freq[i]!=0){
             hs.add(freq[i]);
             }
