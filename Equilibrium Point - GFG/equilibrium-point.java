4c3f4febfb73b6c39699c8304d873c95e059bb0a
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-09-23 23:26:29 +0530

    Added solution - LeetHub

---
+++ Equilibrium Point - GFG/equilibrium-point.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            
+            //taking input n
+            int n = Integer.parseInt(br.readLine().trim());
+            long arr[] = new long[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //adding elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(inputLine[i]);
+            }
+
+            Solution ob = new Solution();
+            
+            //calling equilibriumPoint() function
+            System.out.println(ob.equilibriumPoint(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+      public static int equilibriumPoint(long arr[], int n) {
+          int left=0;
+        int right=n-1;
+        long leftsum=arr[left];
+        long rightsum=arr[right];
+        while(left!=right){
+            if(leftsum<rightsum){
+                leftsum+=arr[++left];
+            }
+            else if(rightsum<leftsum){
+                rightsum+=arr[--right];
+            }
+            else{
+                leftsum+=arr[++left];
+                rightsum+=arr[--right];
+            }
+        }
+        // System.out.println(leftsum);
+        // System.out.println(rightsum);
+        
+        if(leftsum==rightsum) return left+1;
+        return -1;
+          }
+}
