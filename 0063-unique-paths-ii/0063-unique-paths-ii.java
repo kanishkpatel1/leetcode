74a402b868ca179995d1ade255b03d3671a761d5
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-11-01 08:46:35 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (61.71%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -31,12 +31,6 @@ class Solution {
                 
             }
         }
-         for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
             return dp[arr.length-1][arr[0].length-1];
 
     }
