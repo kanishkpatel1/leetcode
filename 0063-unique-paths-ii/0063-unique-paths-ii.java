0ba221d5539d9b82bf0cd25f5b849638df153375
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-11-01 08:46:06 +0530

    Time: 23 ms (5.63%), Space: 43.1 MB (6.51%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+ public static int uniquePathsWithObstacles(int[][] arr) {
+        if(arr[0][0]==1){
+            return 0;
+        }
+         int dp[][]=new int[arr.length][arr[0].length];
+         for(int i=0;i<arr.length;i++){
+            if(arr[i][0]==1){
+                break;
+            }
+            else{
+                dp[i][0]=1;
+            }
+         }
+         for(int i=0;i<arr[0].length;i++){
+            if(arr[0][i]==1){
+                break;
+            }
+            else{
+                dp[0][i]=1;
+            }
+         }
+        for(int i=1;i<arr.length;i++){
+            for(int j=1;j<arr[0].length;j++){
+                    if(arr[i][j]==1){
+                       dp[i][j]=0; 
+                    }
+                    else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                    }
+                
+            }
+        }
+         for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+            return dp[arr.length-1][arr[0].length-1];
+
+    }
+}
