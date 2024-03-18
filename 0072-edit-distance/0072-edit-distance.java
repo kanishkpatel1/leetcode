5f861767602e87379f135ac2a8a62f08f514e6f5
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2024-03-18 14:55:45 +0530

    Time: 5 ms (68.69%), Space: 45.2 MB (21.47%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,26 +1,18 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int r=word1.length();
-        int c=word2.length();
-        
-        int dp[][]=new int[r+1][c+1];
-        // fill the first row and first col with index values because of diff 
-        for(int i=1;i<=c;i++)  dp[0][i]=i; //first row
-        for(int i=1;i<=r;i++)  dp[i][0]=i;
-        
-        for(int i=1;i<=r;i++){
-            for(int j=1;j<=c;j++){
-                if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1]; // condition for same character in both strings
-                }
-                else{
-                int top=dp[i-1][j];
-                int topleft=dp[i-1][j-1];
-                int left=dp[i][j-1];
-                dp[i][j]=Math.min(top,Math.min(topleft,left))+1;
-                }
+        int row=word1.length()+1;
+        int col=word2.length()+1;
+        int dp[][]=new int[row][col];
+        for(int i=0;i<col;i++) dp[0][i]=i;       // fill the first row
+        for(int i=0;i<row;i++) dp[i][0]=i; // fill the first cool
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1))
+                   dp[i][j]=dp[i-1][j-1]; 
+                else
+                    dp[i][j]=Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1]))+1;
             }
         }
-     return dp[r][c];
+        return dp[row-1][col-1];
     }
 }
